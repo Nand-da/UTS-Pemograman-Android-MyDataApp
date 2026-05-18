9df0229dc4d56393941078d25c2fce18e5a41499
murfidnurhadi <murfidnurhadi8@gmail.com>
2026-05-18 07:43:09 +0700

    UTS-Pemograman-Android-MyDataApp

---
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -0,0 +1,85 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class DashboardActivity extends AppCompatActivity {
+    TextView tvWelcome;
+    EditText etNim, etNama, etProdi,
+            etKelas, etAlamat, etEmail;
+    Button btnTambah, btnLogout;
+    ListView listView;
+    ArrayList<String> dataList;
+    ArrayAdapter<String> adapter;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        tvWelcome = findViewById(R.id.tvWelcome);
+        etNim = findViewById(R.id.etNim);
+        etNama = findViewById(R.id.etNama);
+        etProdi = findViewById(R.id.etProdi);
+        etKelas = findViewById(R.id.etKelas);
+        etAlamat = findViewById(R.id.etAlamat);
+        etEmail = findViewById(R.id.etEmail);
+        btnTambah = findViewById(R.id.btnTambah);
+        btnLogout = findViewById(R.id.btnLogout);
+        listView = findViewById(R.id.listView);
+        sharedPreferences =
+                getSharedPreferences("LOGIN", MODE_PRIVATE);
+        String username =
+                sharedPreferences.getString("username", "");
+        tvWelcome.setText("Selamat Datang " + username);
+        dataList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dataList
+        );
+        listView.setAdapter(adapter);
+        btnTambah.setOnClickListener(v -> {
+            String nim = etNim.getText().toString();
+            String nama = etNama.getText().toString();
+            String prodi = etProdi.getText().toString();
+            String kelas = etKelas.getText().toString();
+            String alamat = etAlamat.getText().toString();
+            String email = etEmail.getText().toString();
+            String data =
+                    "NIM : " + nim +
+                            "\nNama : " + nama +
+                            "\nProdi : " + prodi +
+                            "\nKelas : " + kelas +
+                            "\nAlamat : " + alamat +
+                            "\nEmail : " + email;
+            dataList.add(data);
+            adapter.notifyDataSetChanged();
+            etNim.setText("");
+            etNama.setText("");
+            etProdi.setText("");
+            etKelas.setText("");
+            etAlamat.setText("");
+            etEmail.setText("");
+        });
+        btnLogout.setOnClickListener(v -> {
+            SharedPreferences.Editor editor =
+                    sharedPreferences.edit();
+            editor.clear();
+            editor.apply();
+            Intent intent =
+                    new Intent(DashboardActivity.this,
+                            MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.InputType;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etUsername, etPassword;
+    Button btnLogin, btnCancel;
+    ImageView imgShow;
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCancel = findViewById(R.id.btnCancel);
+        imgShow = findViewById(R.id.imgShow);
+        sharedPreferences =
+                getSharedPreferences("LOGIN", MODE_PRIVATE);
+
+        btnLogin.setOnClickListener(v -> {
+            String username =
+                    etUsername.getText().toString();
+            String password =
+                    etPassword.getText().toString();
+            if(username.isEmpty() || password.isEmpty()){
+                Toast.makeText(
+                        MainActivity.this,
+                        "Data tidak boleh kosong",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+            else if(username.equals("Murfid")
+                    && password.equals("murfid123")){
+                SharedPreferences.Editor editor =
+                        sharedPreferences.edit();
+                editor.putBoolean("isLoggedIn", true);
+                editor.putString("username", username);
+                editor.apply();
+                Intent intent =
+                        new Intent(MainActivity.this,
+                                DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Username atau Password Salah",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        btnCancel.setOnClickListener(v -> {
+            etUsername.setText("");
+            etPassword.setText("");
+        });
+        imgShow.setOnClickListener(v -> {
+            if(etPassword.getInputType() ==
+                    (InputType.TYPE_CLASS_TEXT |
+                            InputType.TYPE_TEXT_VARIATION_PASSWORD)){
+                etPassword.setInputType(
+                        InputType.TYPE_CLASS_TEXT |
+                                InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                );
+            } else {
+                etPassword.setInputType(
+                        InputType.TYPE_CLASS_TEXT |
+                                InputType.TYPE_TEXT_VARIATION_PASSWORD
+                );
+            }
+            etPassword.setSelection(
+                    etPassword.getText().length()
+            );
+        });
+    }
+}
